436ae0d03822a5e0f8e4540fcb34b3a221c2158b
Renan Santos <dosantosrenansilva@gmail.com>
2022-09-03 09:12:52 -0300

    Criado um programa que calcula a area de um triangulo

---
+++ AreaDeUmTriangulo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace AreaDeUmTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Digite o valor dos lados do triangulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.WriteLine("Digite o valor dos lados do triangulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area();
+
+            double areaY = y.Area();
+
+            Console.WriteLine("O valor da área de X é: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("O valor da área de Y é: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("A maior área é a de X");
+            }
+            else if(areaY > areaX)
+            {
+                Console.WriteLine("A maior área é a de Y");
+            }
+            else
+            {
+                Console.WriteLine("Os valores das áreas são iguais");
+            }
+        }
+    }
+}
